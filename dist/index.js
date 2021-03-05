81b1dddf577cd009164fe3c2ab112a1e658fe49c
Alex Iglesias <servicios@brota.me>
2021-03-05 13:50:12 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,2 @@
-"use strict";
-document.addEventListener('DOMContentLoaded', () => {
-    const name = 'John Doe';
-    helloStarter(name);
-});
-const helloStarter = (name) => {
-    console.log(`Hello ${name}!`);
-    console.log('Is this a short code snippet? Try compiling this file with Typescript by running the "tsc" command on your terminal.');
-    console.log('Is this a big project? Try bundling and minifying the files with ESBuild by adding them in the "build.js" config file and running the "npm run build" command on your terminal.');
-};
+(()=>{document.addEventListener("DOMContentLoaded",()=>{e("John Doe")});var e=n=>{console.log(`Hello ${n}!`);console.log('Is this a short code snippet? Try compiling this file with Typescript by running the "tsc" command on your terminal.'),console.log('Is this a big project? Try bundling and minifying the files with ESBuild by adding them in the "build.js" config file and running the "npm run build" command on your terminal.')};})();
+/*! This comment will be untouched when compiling */
