7c9d0d4ec66a83dcb43e8f7063bd7e95af25caec
Alex Iglesias <servicios@brota.me>
2021-03-05 13:47:35 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('Hello world!');
+document.addEventListener('DOMContentLoaded', () => {
+    const name = 'John Doe';
+    helloStarter(name);
+});
+const helloStarter = (name) => {
+    console.log(`Hello ${name}!`);
+    console.log('Is this a short code snippet? Try compiling this file with Typescript by running the "tsc" command on your terminal.');
+    console.log('Is this a big project? Try bundling and minifying the files with ESBuild by adding them in the "build.js" config file and running the "npm run build" command on your terminal.');
+};
