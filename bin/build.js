32ce6e7b56646573aca2598f29dd048ce6ec6e97
Erik Mejia <erik@erikmejia.com>
2023-02-21 16:21:12 -0400

    fix: remove duplicated testing built file

--- bin/build.js
+++ bin/build.js
@@ -6,7 +6,7 @@ const BUILD_DIRECTORY = 'dist';
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 // Config entrypoint files
-const ENTRY_POINTS = ['src/index.ts', 'src/index.ts'];
+const ENTRY_POINTS = ['src/index.ts'];
 
 // Config dev serving
 const LIVE_RELOAD = !PRODUCTION;
