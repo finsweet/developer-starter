e9c1796ff04f89c9e3385b4b19d6171732c7dd97
Alex Iglesias <alexiglesias@finsweet.com>
2023-03-07 12:58:50 +0100

    build: improve build log

--- bin/build.js
+++ bin/build.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import * as esbuild from 'esbuild';
 import { readdirSync } from 'fs';
-import { join } from 'path';
+import { join, sep } from 'path';
 
 // Config output
 const BUILD_DIRECTORY = 'dist';
@@ -42,28 +42,54 @@ else {
       servedir: BUILD_DIRECTORY,
       port: SERVE_PORT,
     })
-    .then(async ({ port }) => {
-      const buildDirectory = join(process.cwd(), BUILD_DIRECTORY);
-      const files = readdirSync(buildDirectory);
-
-      // Log all served files for easy reference
-      console.table(
-        files
-          .map((file) => {
-            if (file.endsWith('map')) return;
-
-            const location = `http://localhost:${port}/${file}`;
-            const tag = file.endsWith('css')
-              ? `<link href="${location}" rel="stylesheet" type="text/css"/>`
-              : `<script defer src="${location}"></script>`;
-
-            return {
-              'Built File': file,
-              'File Location': location,
-              'Import Suggestion': tag,
-            };
-          })
-          .filter(Boolean)
-      );
+    .then(({ port }) => {
+      logServedFiles(BUILD_DIRECTORY, port);
     });
+}
+
+/**
+ * Logs information about the files that are being served during local development.
+ * @param {string} servedir
+ * @param {number} port
+ */
+function logServedFiles(servedir, port) {
+  /**
+   * Recursively gets all files in a directory.
+   * @param {string} dirPath
+   * @returns {string[]} An array of file paths.
+   */
+  const getFiles = (dirPath) => {
+    const files = readdirSync(dirPath, { withFileTypes: true }).map((dirent) => {
+      const path = join(dirPath, dirent.name);
+      return dirent.isDirectory() ? getFiles(path) : path;
+    });
+
+    return files.flat();
+  };
+
+  const files = getFiles(servedir);
+
+  const filesInfo = files
+    .map((file) => {
+      if (file.endsWith('.map')) return;
+
+      // Normalize path and create file location
+      const paths = file.split(sep);
+      paths[0] = `http://localhost:${port}`;
+
+      const location = paths.join('/');
+
+      // Create import suggestion
+      const tag = location.endsWith('.css')
+        ? `<link href="${location}" rel="stylesheet" type="text/css"/>`
+        : `<script defer src="${location}"></script>`;
+
+      return {
+        'File Location': location,
+        'Import Suggestion': tag,
+      };
+    })
+    .filter(Boolean);
+
+  console.table(filesInfo);
 }
