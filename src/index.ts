a602718ac96db48b6fd7fd47840bdd7ae72dea44
Alex Iglesias <alexiglesias@finsweet.com>
2024-06-07 15:16:30 +0200

    fixed my very bad code

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name: number = 'John Doe';
+  const name = 'John Doe';
   greetUser(name);
 });
